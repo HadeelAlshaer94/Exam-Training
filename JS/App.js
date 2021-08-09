aa552994b777c9432038e65ca1dbe50efde6c882
HadeelAlshaer94 <Hadeelalshaer94@outlook.com>
2021-08-09 05:18:11 +0300

    Nav-Bar

---
+++ JS/App.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// For Sticky Header from w3school
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
